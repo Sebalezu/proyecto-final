c1cef0574563c514dbc735821a66ac2ec4ec577e
MaxCarreno <jujomaca2@gmail.com>
2025-12-12 01:39:39 -0500

    Creación de las clases producto, ProductoAdapter, GlobalVars y CarritoAdapter.

---
+++ app/src/main/java/com/example/proyectofinal/GlobalVars.java
@@ -0,0 +1,7 @@
+package com.example.proyectofinal;
+
+import java.util.HashMap;
+
+public class GlobalVars {
+    public static HashMap<String, Producto> carrito = new HashMap<>();
+}
--- app/src/main/java/com/example/proyectofinal/activity_carrito.java
+++ app/src/main/java/com/example/proyectofinal/activity_carrito.java
@@ -15,6 +15,10 @@ public class activity_carrito extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_carrito);
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }

d0ef7ebe02a0ab4c22454ece7476c86a633a9da3
MaxCarreno <jujomaca2@gmail.com>
2025-12-12 00:39:43 -0500

    Creación de las ventanas Producto y Carrito.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 }
